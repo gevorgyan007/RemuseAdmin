1c969e4ddf3184cb0cb687718defa4fb8bcc54e6
Taron <info.gevorgyan007@gmail.com>
2019-07-07 00:39:21 +0400

    Add project files.

---
+++ Repositories/AppRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RemuseWebApplication.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RemuseWebApplication.Repositories
+{
+    public class AppRepository : DbContext
+    {
+        public AppRepository(DbContextOptions<AppRepository> options)
+          : base(options)
+        {
+
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=AppDatabase.db");
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Music> Musics { get; set; }
+    }
+}
